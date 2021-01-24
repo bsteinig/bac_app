e70bbbd2ce08e0bfffd77c9cf2484d2753db5c47
Ben Steinig <bsteinig@umich.edu>
2021-01-24 15:15:17 -0500

    removed unused variables

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,13 @@
 import React from 'react' 
 import Counter from './counter'
 import Info from './info'
+import Bg from "../assets/bg.png"
 
 function Dashboard({userData}) {
 
     return (
-        <div>
+        <div className="form-bg"
+        style={{ backgroundImage: `url(${Bg})`}}>
             <h1 className = "title t3">BAC Track</h1>
             <Counter userData={userData}/>
             <Info />
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Counter from './components/counter'
 
 ReactDOM.render(
   <React.StrictMode>
